6c9d2459547bef68e7ec2ba378372a0203c6fec9
hd <hoangduydao94@gmail.com>
2025-03-13 14:45:32 +0100

    Premier commit

---
+++ src/main/java/com/example/Projet2025ProgDistrib/Projet2025ProgDistribApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projet2025ProgDistrib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet2025ProgDistribApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet2025ProgDistribApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Projet2025ProgDistrib/Projet2025ProgDistribApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projet2025ProgDistrib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet2025ProgDistribApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
